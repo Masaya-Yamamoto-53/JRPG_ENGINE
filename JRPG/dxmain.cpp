ca80b38191c2981344a5576042c2a8bd1ad56b16
masaya <masaya.y.53@gmail.com>
2026-01-15 13:32:48 +0900

    MAIN関数を追加

---
+++ JRPG/dxmain.cpp
@@ -0,0 +1,16 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(
+      _In_ HINSTANCE hInstance
+    , _In_opt_ HINSTANCE hPrevInstance
+    , _In_ LPSTR lpCmdLine
+    , _In_ int nCmdShow
+) {
+    // Suppress unused parameter warnings
+    (void)hInstance;
+    (void)hPrevInstance;
+    (void)lpCmdLine;
+    (void)nCmdShow;
+
+    return 0;
+}
