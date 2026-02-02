ad444c9a9c427be0d0264a2e229a1c124d49c585
masaya <masaya.y.53@gmail.com>
2026-02-02 15:17:23 +0900

    不要なコメントを削除

--- JRPG/EnemyAnimationStrategy.cpp
+++ JRPG/EnemyAnimationStrategy.cpp
@@ -8,42 +8,4 @@ EnemyAnimationStrategy::EnemyAnimationStrategy() {
          1,  1,  1,  1,  1,  1,  1,  1
     };
 }
-/*
-const CharacterImage& EnemyAnimationStrategy::getImage() const {
-    return m_images[m_animIndex];
-}
 
-void EnemyAnimationStrategy::update(Direction useDir, bool isMoving, bool running) {
-    if(isMoving) {
-        m_frame = (m_frame + 1) % imgPattern.size();
-    }
-    else {
-        m_frame = 0;
-    }
-    if (useDir != Direction::None) {
-        m_animIndex = calcAnimIndex(useDir, running);
-        m_animIndex += imgPattern[m_frame];
-    }
-}
-
-int EnemyAnimationStrategy::calcAnimIndex(Direction dir, bool running) const {
-    int base = 0;
-    switch (dir) {
-    case Direction::Down:
-        base = m_down;
-        break;
-    case Direction::Up:
-        base = m_up;
-        break;
-    case Direction::Left:
-        base = running ? m_leftRun  : m_left;
-        break;
-    case Direction::Right:
-        base = running ? m_rightRun : m_right;
-        break;
-    default:
-        break;
-    }
-    return base;
-}
-*/
--- JRPG/PlayerAnimationStrategy.cpp
+++ JRPG/PlayerAnimationStrategy.cpp
@@ -8,42 +8,4 @@ PlayerAnimationStrategy::PlayerAnimationStrategy() {
          1,  0,  0,  0,  0,  0,  0,  0
     };
 }
-/*
-const CharacterImage& PlayerAnimationStrategy::getImage() const {
-    return m_images[m_animIndex];
-}
 
-void PlayerAnimationStrategy::update(Direction useDir, bool isMoving, bool running) {
-    if(isMoving) {
-        m_frame = (m_frame + 1) % imgPattern.size();
-    }
-    else {
-        m_frame = 0;
-    }
-    if (useDir != Direction::None) {
-        m_animIndex = calcAnimIndex(useDir, running);
-        m_animIndex += imgPattern[m_frame];
-    }
-}
-
-int PlayerAnimationStrategy::calcAnimIndex(Direction dir, bool running) const {
-    int base = 0;
-    switch (dir) {
-    case Direction::Down:
-        base = m_down;
-        break;
-    case Direction::Up:
-        base = m_up;
-        break;
-    case Direction::Left:
-        base = running ? m_leftRun  : m_left;
-        break;
-    case Direction::Right:
-        base = running ? m_rightRun : m_right;
-        break;
-    default:
-        break;
-    }
-    return base;
-}
-*/
